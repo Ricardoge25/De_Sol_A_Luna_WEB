02904964bb2c80b999b7b8c622f8bdd87174d18d
Ricardoge25 <gonzalezricardo161@gmail.com>
2025-09-17 21:27:58 -0500

    Inicialización del proyecto con React + Vite + Js + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-5xl text-red-500 ">De Sol a Luna Web Oficial</h1>
+      </div>      
+    </>
+  )
+}
+
+export default App
