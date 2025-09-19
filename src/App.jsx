d6773559fa409f0c1468db391380d03f58b9a1b2
Ricardoge25 <gonzalezricardo161@gmail.com>
2025-09-18 22:20:41 -0500

    Avance de las secciones hasta 'Galería Visual'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
 import Header from './components/Header'
+import Hero from './components/Hero'
+import Sinopsis from './components/Sinopsis'
+import Gallery from './components/Gallery'
 
 function App() {
   return (
     <>
         <Header />
+        <main className='pt-20' >
+          {/* Aquí van los demás componentes */}
+        <Hero />
+        <Sinopsis />
+        <Gallery />
+        </main>
     </>
   )
 }
